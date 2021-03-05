2f3d1a8e96bfcffeed1dfd6dd513bd1644da6ec8
sho9805 <sho0905@naver.com>
2021-03-05 17:43:36 +0900

    업로드

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+function nowMenu(menu){
+    console.log(menu);
+    $("ul.menu>li").eq(menu).addClass("active");
+    $(".bar").css({left:200*menu}); //menu 0 1 2 3 4
+
+    //$("ul.menu>li").hover(function(){},function(){}); mouseenter / mouseleave
+    $("ul.menu>li").hover(function(){
+      var menuLi = $(this).index();
+      $(".bar").stop().animate({left:menuLi*200},100);
+    },function(){
+      $(".bar").stop().animate({left:menu*200},100);
+    });
+  }
+© 2021 GitHub, Inc.
